7ba70a8ca7e071982f06ec369d54d5105ca7f25f
wnonna13 <wnonna@yahoo.com>
2023-04-22 14:34:11 +0900

    Add formal pseudocode

--- Tutorials/lesson_2/7.ps2.rb
+++ Tutorials/lesson_2/7.ps2.rb
@@ -8,4 +8,17 @@
 
 
 ##########
+START
+#Given an array of strings called "strings"
 
+SET iterator = 1
+SET saved_string = string of concatenated strings
+SET space = " "
+
+WHILE iterator <= length of strings
+    SET current_string = string at space "iterator"
+    saved_string += space + current_string
+
+PRINT saved_string
+
+END
