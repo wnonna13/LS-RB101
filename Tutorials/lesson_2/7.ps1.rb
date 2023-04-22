200f22edfe1c8046f2a9101ee1fad48a8ee23e88
wnonna13 <wnonna@yahoo.com>
2023-04-22 14:18:16 +0900

    Add formal pseudocode

--- Tutorials/lesson_2/7.ps1.rb
+++ Tutorials/lesson_2/7.ps1.rb
@@ -3,3 +3,14 @@
 # Add two numbers together
 # Save to a number
 # Return saved number
+
+START
+
+SET num1 = user_input1
+SET num2 = user_input2
+
+SET saved_number = num1 + num2
+
+PRINT saved_number
+
+END
