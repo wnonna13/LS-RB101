6b8dc3f59020e953a06d1ae997664e1dbcca5935
wnonna13 <wnonna@yahoo.com>
2023-04-22 14:23:16 +0900

    Add code

--- Tutorials/lesson_2/7.ps1.rb
+++ Tutorials/lesson_2/7.ps1.rb
@@ -4,13 +4,23 @@
 # Save to a number
 # Return saved number
 
-START
+###########################
 
-SET num1 = user_input1
-SET num2 = user_input2
+#START
 
-SET saved_number = num1 + num2
+#SET num1 = user_input1
+#SET num2 = user_input2
 
-PRINT saved_number
+#SET saved_number = num1 + num2
 
-END
+#PRINT saved_number
+
+#END
+
+#########################
+
+def sum(num1, num2)
+    result = num1.to_i + num2.to_i
+end
+
+puts sum(1,2)
