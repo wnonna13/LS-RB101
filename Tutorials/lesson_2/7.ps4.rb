c4865e7a817f28d66d8db045a1481ff484db45b3
wnonna13 <wnonna@yahoo.com>
2023-04-22 16:55:35 +0900

    Add instr

---
+++ Tutorials/lesson_2/7.ps4.rb
@@ -0,0 +1,4 @@
+# A method that dermines the index of the 3rd occurence in a string. 
+# Example:
+# If given character is 'x' and string is 'axbxcxdxex' it should return '6' (The 3rd index of 'x')
+# If not 3 times, return nil.
