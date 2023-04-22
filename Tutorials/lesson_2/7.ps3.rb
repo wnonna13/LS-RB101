dd2b29d3c873334a0d097660d53d6613d91d70fa
wnonna13 <wnonna@yahoo.com>
2023-04-22 15:50:18 +0900

    Add instr

---
+++ Tutorials/lesson_2/7.ps3.rb
@@ -0,0 +1,3 @@
+# Create a method that takes an array of integers, and returns a new array with every other element from the original array, starting with the first element.
+
+# Example : every_other([1,4,7,2,5]) # => [1,7,5]
