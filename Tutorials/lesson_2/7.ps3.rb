104b5861895540b1d2b6ccfa80744ede42fbc8b7
wnonna13 <wnonna@yahoo.com>
2023-04-22 16:34:12 +0900

    Add pseudocode

--- Tutorials/lesson_2/7.ps3.rb
+++ Tutorials/lesson_2/7.ps3.rb
@@ -1,3 +1,13 @@
 # Create a method that takes an array of integers, and returns a new array with every other element from the original array, starting with the first element.
 
+# Create a new array variable
+# Iterate through the array one by one
+# If the current iteration index is divisable by 2
+    # Add current iteration value to the new array variable
+# If not divisible by two
+    # Skip
+# Return saved new array
+
 # Example : every_other([1,4,7,2,5]) # => [1,7,5]
+
+######################################
