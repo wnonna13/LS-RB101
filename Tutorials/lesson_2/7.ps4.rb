1bb047c134b70b80f6facf3bad4c53adbf2eaff7
wnonna13 <wnonna@yahoo.com>
2023-04-23 00:18:07 +0900

    Add partial code

--- Tutorials/lesson_2/7.ps4.rb
+++ Tutorials/lesson_2/7.ps4.rb
@@ -22,23 +22,44 @@
 
 #################
 
-START
+#START
 # Given a string called "string"
-SET iterator = 1
-SET counter = 0
-SET char = character targeted
+#SET iterator = 1
+#SET counter = 0
+#SET char = character targeted
 
-WHILE iterator <= length of string || counter <= 3
-    IF current iteration == char
-        counter += 1
-    ELSE
-        nil
-    iterator += 1
+#WHILE iterator <= length of string || counter <= 3
+#    IF current iteration == char
+#        counter += 1
+#    ELSE
+#        nil
+#    iterator += 1
 
-IF counter === 3
-    PRINT index of last iteration
-ELSE
-    PRINT nil
+#IF counter === 3
+#    PRINT index of last iteration
+#ELSE
+#    PRINT nil
 
+#END
 
-END
+#################
+
+def get_third(string, char)
+    counter = nil
+    
+    string.each_with_index do |e, i|
+        if counter <= 3
+            if e == char
+                counter += 1
+            else 
+                nil
+            end
+        else
+            i
+        end
+    
+    end
+
+end
+
+puts get_third("axbxcxdxex", "x")
