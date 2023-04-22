c72e00f45e665a6b7e12b80e145f26557dc7dcc6
wnonna13 <wnonna@yahoo.com>
2023-04-22 16:58:33 +0900

    Add pseudocode

--- Tutorials/lesson_2/7.ps4.rb
+++ Tutorials/lesson_2/7.ps4.rb
@@ -2,3 +2,22 @@
 # Example:
 # If given character is 'x' and string is 'axbxcxdxex' it should return '6' (The 3rd index of 'x')
 # If not 3 times, return nil.
+
+#################
+
+#Input: string, character
+#Output: integer of 3rd instance of character OR nil
+
+# Iterate through a given array containing a string
+# Set a counter to count the 3rd instance of a character
+# In while loop
+    # If current iteration = character
+        # counter += 1
+    # Else, pass
+# If counter == 3
+    # Stop. 
+    # Return index number
+# Else
+    # Return nil 
+
+#################
