ace1777be910687c4b70e9c4e6a3452e85f85b32
wnonna13 <wnonna@yahoo.com>
2023-04-22 14:27:59 +0900

    Add informal pseudocode

---
+++ Tutorials/lesson_2/7.ps2.rb
@@ -0,0 +1,11 @@
+#A method that takes an array of strings, and returns a string that is all those strings concatenated together.
+
+#Create a new string
+#Iterate through the array. 
+    # Add a space
+    # Add each new value to the string
+# Return saved new string
+
+
+##########
+
