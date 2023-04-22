512294258f0adfa6f424e8307efafa2cf5899bd7
wnonna13 <wnonna@yahoo.com>
2023-04-22 16:49:28 +0900

    Add code

--- Tutorials/lesson_2/7.ps3.rb
+++ Tutorials/lesson_2/7.ps3.rb
@@ -12,20 +12,37 @@
 
 ######################################
 
-START
+#START
 #Given a collection of integers in an "array"
 
-SET iterator = 0
-SET saved_array = array with only even indexed numbers
+#SET iterator = 0
+#SET saved_array = array with only even indexed numbers
 
-WHILE iterator <= length of array
-    IF current index iteration % 2 == 0
-        save number to saved_array
-    ELSE
-        skip 
-    iterator = iterator + 1
+#WHILE iterator <= length of array
+#    IF current index iteration % 2 == 0
+#        save number to saved_array
+#    ELSE
+#        skip 
+#   iterator = iterator + 1
 
-PRINT saved_array
+#PRINT saved_array
 
+#END
 
-END
+###########################################
+
+def every_other(array)
+    saved_array = []
+
+    array.each_with_index do |e, i|
+        if i % 2 == 0
+            saved_array.append(e)
+        else
+            nil
+        end
+    end
+
+    saved_array.inspect
+end
+
+puts every_other([1, 3, 4, 5, 7])
