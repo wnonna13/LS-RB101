5672bac8db12956f822607c8665bcfcd645c11f0
wnonna13 <wnonna@yahoo.com>
2023-04-22 13:27:36 +0900

    Add pseudocode example

---
+++ Tutorials/lesson_2/7.1.rb
@@ -0,0 +1,10 @@
+#Given a collection of integers.
+
+#Iterate through the collection one by one. 
+    #- save the first value as the starting value. 
+    # - for each iteration, compare the saved value with the current value
+    #- if the saved value is greater, or it's the same
+        # - move to the next value in the collection
+    # - otherwise if the current value is greater
+        # - reassign the saved value as the current value
+# After iterating through the collection, return the saved value
