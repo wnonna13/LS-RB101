c0d7e4d824bd05b35c4b150718078126a0a2c357
wnonna13 <wnonna@yahoo.com>
2023-04-24 19:11:06 +0900

    Add instructions

---
+++ Tutorials/lesson_2/7.ps5.rb
@@ -0,0 +1,4 @@
+# Given a method that takes two arrays of numbers, 
+# returns the result of merging the arrays, 
+# First array elements are even indexes of returned array
+# Second array elements are odd indexes of returned array
