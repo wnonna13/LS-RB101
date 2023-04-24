9f26d12ec84bc68e85c5214c20bbc1e688e8be3d
wnonna13 <wnonna@yahoo.com>
2023-04-24 19:11:55 +0900

    Identify input, output

--- Tutorials/lesson_2/7.ps5.rb
+++ Tutorials/lesson_2/7.ps5.rb
@@ -2,3 +2,15 @@
 # returns the result of merging the arrays, 
 # First array elements are even indexes of returned array
 # Second array elements are odd indexes of returned array
+
+###########
+
+# input
+# - array 1 
+# - array 2
+
+# output
+# - returned array
+# *** array 1 is even index, array 2 is odd index
+
+###########
