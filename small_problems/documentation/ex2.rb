9324acdd670cf9d045f27f5ff7cb244a826e60fa
wnonna13 <wnonna@yahoo.com>
2023-04-26 18:51:45 +0900

    Add solution

--- small_problems/documentation/ex2.rb
+++ small_problems/documentation/ex2.rb
@@ -2,7 +2,7 @@
 
 require 'date'
 
-puts Date.civil
-puts Date.civil(2016)
-puts Date.civil(2016, 5)
-puts Date.civil(2016, 5, 13)
+puts Date.civil # -4712-01-01 
+puts Date.civil(2016) # 2016-01-01
+puts Date.civil(2016, 5) #2016-05-01
+puts Date.civil(2016, 5, 13) #2016-05-13
