deb296ba18658978b5589b4a1c4de5ce4550aac9
wnonna13 <wnonna@yahoo.com>
2023-04-26 18:53:41 +0900

    Add explanation

--- small_problems/documentation/ex2.rb
+++ small_problems/documentation/ex2.rb
@@ -2,7 +2,7 @@
 
 require 'date'
 
-puts Date.civil # -4712-01-01 
-puts Date.civil(2016) # 2016-01-01
-puts Date.civil(2016, 5) #2016-05-01
-puts Date.civil(2016, 5, 13) #2016-05-13
+puts Date.civil # -4712-01-01 #Without specific parameters, ruby wills tart from 4712 BC.
+puts Date.civil(2016) # 2016-01-01 # A year is specified, but since there is no month or day specified, it takes 01 / 01 as default
+puts Date.civil(2016, 5) #2016-05-01 # Month and year are specified. But, since there is no date specified, it takes the 1st date.
+puts Date.civil(2016, 5, 13) #2016-05-13 # Year, month and day are specified. Thus, ruby prints it out as written.
