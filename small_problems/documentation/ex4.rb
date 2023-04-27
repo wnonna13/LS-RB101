da511bb0daf1dce87def2e5fe7a9de84d1690d5c
wnonna13 <wnonna@yahoo.com>
2023-04-27 18:34:28 +0900

    Add instructions

---
+++ small_problems/documentation/ex4.rb
@@ -0,0 +1,5 @@
+a = [1, 4, 8, 11, 15, 19]
+
+#The Array#bsearch method is used to search ordered Arrays more quickly than #find and #select can. Assume you have the following code:
+
+#How would you search this Array to find the first element whose value exceeds 8?
