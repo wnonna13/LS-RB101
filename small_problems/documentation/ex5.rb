8273cac3c117d379e12b3295a48996c1760e79f9
wnonna13 <wnonna@yahoo.com>
2023-04-27 19:06:41 +0700

    Add explanation to %w

--- small_problems/documentation/ex5.rb
+++ small_problems/documentation/ex5.rb
@@ -1,6 +1,6 @@
 #What do each of these puts statements output?
 
-a = %w(a b c d e)
+a = %w(a b c d e) # -> a = [a, b, c, d, e]
 puts a.fetch(7)
 puts a.fetch(7, 'beats me')
 puts a.fetch(7) { |index| index**2 }
