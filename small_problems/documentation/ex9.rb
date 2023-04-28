9ba5a9ab543c27c39824212f5c8e7c7da2b36861
wnonna13 <wnonna@yahoo.com>
2023-04-28 23:10:09 +0900

    Add documentation

--- small_problems/documentation/ex9.rb
+++ small_problems/documentation/ex9.rb
@@ -2,3 +2,9 @@ require 'yaml'
 MESSAGES = YAML.load_file('calculator_messages.yml')
 
 #Find the documentation for YAML::load_file.
+
+#DOCUMENTATION
+#Load a document from the file located at filepath:
+#YAML.load_file( 'animals.yaml' )
+   #=> ['badger', 'elephant', 'tiger']
+#Link: https://ruby-doc.org/stdlib-1.8.6/libdoc/yaml/rdoc/YAML.html
