3c6be6cd4054e26ca142f17c97129e70602ebf01
wnonna13 <wnonna@yahoo.com>
2023-04-28 18:28:10 +0700

    Add possible solution

--- small_problems/documentation/ex5.rb
+++ small_problems/documentation/ex5.rb
@@ -1,6 +1,6 @@
 #What do each of these puts statements output?
 
 a = %w(a b c d e) # -> a = [a, b, c, d, e]
-puts a.fetch(7)
-puts a.fetch(7, 'beats me')
-puts a.fetch(7) { |index| index**2 }
+puts a.fetch(7) # returns index out of bound
+puts a.fetch(7, 'beats me') # -> returns 'beats me'
+puts a.fetch(7) { |index| index**2 } # -> returns 7 **2 = 49
