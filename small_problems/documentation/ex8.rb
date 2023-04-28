695760cb670c8ca8ff1dcde9439e40409fbc3e24
wnonna13 <wnonna@yahoo.com>
2023-04-28 23:00:40 +0900

    Add file

---
+++ small_problems/documentation/ex8.rb
@@ -0,0 +1,7 @@
+a = [5, 9, 3, 11]
+puts a.min
+
+#Use irb to run the following code:
+
+#Find the documentation for the #min method and change the above code to print the two smallest values in the Array.
+
