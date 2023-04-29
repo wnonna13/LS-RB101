cd33b9183d594d056fb5680a6e872c988a011810
wnonna13 <wnonna@yahoo.com>
2023-04-29 21:05:41 +0900

    Add extra solution

--- small_problems/easy1/ex2.rb
+++ small_problems/easy1/ex2.rb
@@ -16,3 +16,15 @@ puts is_odd?(-8)   # => false
 puts is_odd?(0)    # => false
 puts is_odd?(7)    # => true
 
+puts "***"
+
+def is_odd2?(num)
+    num % 2 != 0
+end
+
+puts is_odd2?(2)    # => false
+puts is_odd2?(5)    # => true
+puts is_odd2?(-17)  # => true
+puts is_odd2?(-8)   # => false
+puts is_odd2?(0)    # => false
+puts is_odd2?(7)    # => true
