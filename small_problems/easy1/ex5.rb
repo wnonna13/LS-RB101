ef25a5ffd4f1adb467d4a63fea5d2ea905402cb8
wnonna13 <wnonna@yahoo.com>
2023-04-30 01:48:31 +0900

    Add partial solution

--- small_problems/easy1/ex5.rb
+++ small_problems/easy1/ex5.rb
@@ -1,6 +1,23 @@
 #Write a method that will take a short line of text, and print it within a box.
 
-print_in_box('To boldly go where no one has gone before.')
+#Pseudocode
+
+#Input: string
+#output: string in a box
+
+def print_in_box(string)
+    <<-TEXT
++--------------------------------------------+
+|                                            |
+|  #{string}                   |
+|                                            |
++--------------------------------------------+
+    TEXT
+end
+
+
+
+puts print_in_box('To boldly go where no one has gone before.')
 
 #Example:
 #+--------------------------------------------+
