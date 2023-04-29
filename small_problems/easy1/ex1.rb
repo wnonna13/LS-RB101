58e31997910100bbdc0b239e5d6d20c6c62c5c0f
wnonna13 <wnonna@yahoo.com>
2023-04-29 18:59:22 +0900

    Add solution

--- small_problems/easy1/ex1.rb
+++ small_problems/easy1/ex1.rb
@@ -1,1 +1,13 @@
 #Write a method that takes two arguments, a string and a positive integer, and prints the string as many times as the integer indicates.
+
+
+def repeat(str, num)
+    iterator = 0
+    while iterator < num.to_i
+        puts str
+        iterator += 1
+    end
+end
+
+repeat('Hello', 3)
+repeat('yo', 5)
