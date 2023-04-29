8bed290ce9c7278bfabc20a0fa63811c33ebc856
wnonna13 <wnonna@yahoo.com>
2023-04-30 01:36:11 +0900

    Add instructions

---
+++ small_problems/easy1/ex5.rb
@@ -0,0 +1,10 @@
+#Write a method that will take a short line of text, and print it within a box.
+
+print_in_box('To boldly go where no one has gone before.')
+
+#Example:
+#+--------------------------------------------+
+#|                                            |
+#| To boldly go where no one has gone before. |
+#|                                            |
+#+--------------------------------------------+
