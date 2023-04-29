be081bd438a6ae78282be03328a8f637df88b9aa
wnonna13 <wnonna@yahoo.com>
2023-04-30 01:10:38 +0900

    Add official solution

--- small_problems/easy1/ex3.rb
+++ small_problems/easy1/ex3.rb
@@ -33,3 +33,21 @@ puts stringy(6) == '101010'
 puts stringy(9) == '101010101'
 puts stringy(4) == '1010'
 puts stringy(7) == '1010101'
+
+puts "***"
+
+def stringy2(num)
+    numbers = []
+
+    num.times do |x|
+        x = x.even? ? 1: 0
+        numbers << x
+    end
+
+    numbers.join
+end
+
+puts stringy2(6) == '101010'
+puts stringy2(9) == '101010101'
+puts stringy2(4) == '1010'
+puts stringy2(7) == '1010101'
