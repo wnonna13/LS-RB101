81e2b91de91814f49177098e91cc8f2f3965a3d1
wnonna13 <wnonna@yahoo.com>
2023-04-30 01:31:44 +0900

    Add pseudocode, solution

--- small_problems/easy1/ex4.rb
+++ small_problems/easy1/ex4.rb
@@ -1,5 +1,22 @@
 #Write a method that takes two arguments, a positive integer and a boolean, and calculates the bonus for a given salary. If the boolean is true, the bonus should be half of the salary. If the boolean is false, the bonus should be 0.
 
+#Pseudocode
+
+#input = positive integer (salary), boolean (true/false)
+#output = bonus (positive integer OR 0)
+
+#Create new varibale = bonus
+# If boolean == true
+    # bonus = salary / 2 
+# else 
+    # bonus = 0
+
+
+def calculate_bonus(salary, bool)
+    bool ? (salary/2) : 0
+end
+
+
 puts calculate_bonus(2800, true) == 1400
 puts calculate_bonus(1000, false) == 0
 puts calculate_bonus(50000, true) == 25000
