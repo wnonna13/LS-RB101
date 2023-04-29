de94a77f4dd2c36c07a688c0b91283e08612d501
wnonna13 <wnonna@yahoo.com>
2023-04-29 21:10:13 +0900

    Add rough pseudocode

--- small_problems/easy1/ex3.rb
+++ small_problems/easy1/ex3.rb
@@ -1,5 +1,23 @@
 #Write a method that takes one argument, a positive integer, and returns a string of alternating 1s and 0s, always starting with 1. The length of the string should match the given integer.
 
+#PSEUDOCODE
+#INPUT: number
+#OUTPUT: string with alternating 1s and 0s
+
+#ROUGH PSUEDOCODE
+# Create a new empty string 
+# Iterater through a while loop using number
+# Use if statement:
+    # If i % 2 == 0, new empty string += 1
+    # else, new empty string += 0
+# Return new empty string
+
+def stringy(num)
+
+end
+
+
+
 puts stringy(6) == '101010'
 puts stringy(9) == '101010101'
 puts stringy(4) == '1010'
