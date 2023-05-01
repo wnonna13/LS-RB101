f4f991bfc205a6e41c25c22aa0a7b5d80abf544c
wnonna13 <wnonna@yahoo.com>
2023-05-01 18:56:22 +0900

    Add pseudocode method

--- small_problems/easy1/ex6.rb
+++ small_problems/easy1/ex6.rb
@@ -1,2 +1,23 @@
 #Write a method that takes a positive integer, n, as an argument, and displays a right triangle whose sides each have n stars. The hypotenuse of the triangle (the diagonal side in the images below) should have one end at the lower-left of the triangle, and the other end at the upper-right.
 
+#PSEUDOCODE
+
+#Input = positive integer
+# Output = right triangle
+
+#Method
+# Create iterator variable
+# Iterate through a while loop
+    # While iterator <= positive integer
+        # space variable = positive integer - iterator
+        # puts "space variable" + "positive integer as *"
+
+
+
+triangle(5)
+
+#    *
+#   **
+#  ***
+# ****
+#*****
