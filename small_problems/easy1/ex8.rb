745afc5a99575c5bf9a5b96970359ae101c8a80a
wnonna13 <wnonna@yahoo.com>
2023-05-02 19:54:07 +0900

    Add pseudocode

--- small_problems/easy1/ex8.rb
+++ small_problems/easy1/ex8.rb
@@ -2,9 +2,30 @@
 
 #Create a simple mad-lib program that prompts for a noun, a verb, an adverb, and an adjective and injects those into a story that you create.
 
-Enter a noun: dog
-Enter a verb: walk
-Enter an adjective: blue
-Enter an adverb: quickly
 
-Do you walk your blue dog quickly? That's hilarious!
+#PSEUDOCODE
+#Input = noun, verb, adj, adv
+#Output = sentence
+
+#METHOD
+# Ask user for noun prompt
+    # Verify it is a string
+    # Save to variable 'noun'
+    # Print nount and ask user if it is correct
+        # If yes, proceed
+        # If no, go back to ask user for prompt
+# Repeat with: verb, adjective, adverb
+# Create message variable
+# Use template literals to save noun, verb, adj, and adverb to message
+# Return message
+
+
+
+#EXAMPLE
+#Enter a noun: dog
+#Enter a verb: walk
+#Enter an adjective: blue
+#Enter an adverb: quickly
+
+#Do you walk your blue dog quickly? That's hilarious!
+
