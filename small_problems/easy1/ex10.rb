f551a19b22080f78be422da595e74920b8935d31
wnonna13 <wnonna@yahoo.com>
2023-05-03 22:14:00 +0900

    Add official solution

--- small_problems/easy1/ex10.rb
+++ small_problems/easy1/ex10.rb
@@ -39,3 +39,12 @@ puts center_of('Launch School') #== ' '
 puts center_of('Launch') #== 'un'
 puts center_of('Launchschool') #== 'hs'
 puts center_of('x') #== 'x'
+
+def center_of2(string)
+  center_index = string.size / 2
+  if string.size.odd?
+    string[center_index]
+  else
+    string[center_index - 1, 2]
+  end
+end
