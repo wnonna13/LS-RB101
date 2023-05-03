aa3e0be1d79321d9be2d2621016eb940fd61c64d
wnonna13 <wnonna@yahoo.com>
2023-05-03 19:50:13 +0900

    Add pseudocode input, output

--- small_problems/easy1/ex10.rb
+++ small_problems/easy1/ex10.rb
@@ -1,5 +1,9 @@
 #Write a method that takes a non-empty string argument, and returns the middle character or characters of the argument. If the argument has an odd length, you should return exactly one character. If the argument has an even length, you should return exactly two characters.
 
+#PSEUDOCODE
+#Input = string
+#Output = one (if string has odd length) OR two characters (if string has even length)
+
 center_of('I love ruby') == 'e'
 center_of('Launch School') == ' '
 center_of('Launch') == 'un'
