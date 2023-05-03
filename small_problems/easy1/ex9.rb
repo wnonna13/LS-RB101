bd7290b6bb092209faa30e3f2d505e853edfd700
wnonna13 <wnonna@yahoo.com>
2023-05-03 19:31:32 +0900

    Add pseudocode method

--- small_problems/easy1/ex9.rb
+++ small_problems/easy1/ex9.rb
@@ -1,5 +1,17 @@
 #Write a method that takes a positive integer as an argument and returns that number with its digits reversed. Examples:
 
+#PSEUDOCODE
+#input = integer
+# output = reversed integer
+
+#METHOD
+# Create a new, empty string
+# Iterate through input string
+    # Get index of each loop 
+    # Added variable = length of string - current index in loop
+    # Add this variable to the empty string
+# Return saved string
+
 reversed_number(12345) == 54321
 reversed_number(12213) == 31221
 reversed_number(456) == 654
