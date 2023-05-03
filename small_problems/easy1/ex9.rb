6f468df3b646d57c45d575dd96abf9d05f2f93cc
wnonna13 <wnonna@yahoo.com>
2023-05-03 19:28:52 +0900

    Add instructions

---
+++ small_problems/easy1/ex9.rb
@@ -0,0 +1,8 @@
+#Write a method that takes a positive integer as an argument and returns that number with its digits reversed. Examples:
+
+reversed_number(12345) == 54321
+reversed_number(12213) == 31221
+reversed_number(456) == 654
+reversed_number(12000) == 21 # No leading zeros in return value!
+reversed_number(12003) == 30021
+reversed_number(1) == 1
