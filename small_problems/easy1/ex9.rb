ab1a289714b4526c44c0cd9b6698bdada7ddb981
wnonna13 <wnonna@yahoo.com>
2023-05-03 19:38:47 +0900

    Check with test cases

--- small_problems/easy1/ex9.rb
+++ small_problems/easy1/ex9.rb
@@ -24,8 +24,8 @@ def reversed_number(num)
 end
 
 puts reversed_number(12345) #== 54321
-#puts reversed_number(12213)# == 31221
-#puts reversed_number(456)# == 654
-#puts reversed_number(12000)# == 21 # No leading zeros in return value!
-#puts reversed_number(12003)# == 30021
-#puts reversed_number(1)# == 1
+puts reversed_number(12213)# == 31221
+puts reversed_number(456)# == 654
+puts reversed_number(12000)# == 21 # No leading zeros in return value!
+puts reversed_number(12003)# == 30021
+puts reversed_number(1)# == 1
