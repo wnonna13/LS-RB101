d292c143a940a3410011dac5d7caac95fe8404d4
wnonna13 <wnonna@yahoo.com>
2023-05-05 03:28:38 -0400

    Add pseudocode

--- small_problems/easy2/ex1.rb
+++ small_problems/easy2/ex1.rb
@@ -1,3 +1,13 @@
 #Build a program that randomly generates and prints Teddy's age. To get the age, you should generate a random number between 20 and 200.
 
+#PSEUDOCODE
+#input = none
+#output = message (containing random age)
+
+#METHOD
+# save age variable as a random number between 20 and 200
+# return message
+
+
+
 #Teddy is 69 years old!
