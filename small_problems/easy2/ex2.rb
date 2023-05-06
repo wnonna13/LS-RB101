86519a0287d923e2891739520e116d994968b372
wnonna13 <wnonna@yahoo.com>
2023-05-06 01:14:59 -0400

    Add pseudocode

--- small_problems/easy2/ex2.rb
+++ small_problems/easy2/ex2.rb
@@ -4,6 +4,21 @@
 
 #Do not worry about validating the input at this time.
 
+#Pseudocode 
+# input = length, width
+# output = Area in square meters, area in square feet 
+
+# METHOD
+# Ask for input length
+    # Validate the number 
+    # Ask if user is happy with it 
+        # If yes, proceed
+        # If no, change 
+# Repeat process - Ask for input width 
+# Calculate area meters squared = (width * length)
+# Convert to feet (1 sq m = 10.76 sq feet)
+# Return message containing meters and feet
+
 #Enter the length of the room in meters:
 #10
 #Enter the width of the room in meters:
