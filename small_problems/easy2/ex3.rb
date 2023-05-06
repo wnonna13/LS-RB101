b4fce3a6e37d67a871f007e1e5b87c9abc282d83
wnonna13 <wnonna@yahoo.com>
2023-05-06 09:52:55 -0400

    Add instructions

---
+++ small_problems/easy2/ex3.rb
@@ -0,0 +1,7 @@
+#Create a simple tip calculator. The program should prompt for a bill amount and a tip rate. The program must compute the tip and then display both the tip and the total amount of the bill.
+
+#What is the bill? 200
+#What is the tip percentage? 15
+
+#The tip is $30.0
+#The total is $230.0
