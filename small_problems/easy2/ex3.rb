17bbd6285a8550073513e4ca05bc1a58a47c1c5b
wnonna13 <wnonna@yahoo.com>
2023-05-06 09:57:01 -0400

    Add psuedocode

--- small_problems/easy2/ex3.rb
+++ small_problems/easy2/ex3.rb
@@ -1,5 +1,26 @@
 #Create a simple tip calculator. The program should prompt for a bill amount and a tip rate. The program must compute the tip and then display both the tip and the total amount of the bill.
 
+# INPUT = bill amount, tip percentage
+# OUPTPUT = tip amount, total bill (with bill + tip added)
+
+# METHOD
+# Ask for bill amount
+    # Validate it is a number
+    # Ask user if this is the correct amount
+        # If yes, proceed
+        # If no, go back to 1st step
+# Repeat with tip percentage
+# Create new saved variable = tip amount
+# Create new saved variable = total after tip amount
+# Calculate tip amount 
+    # tip percentage * bill amount / 100
+# Calculate total after tip amount
+    # tip amount + bill amount 
+# Create message:
+    # The tip is : $tip
+    # The total is : $total 
+# Return message 
+
 #What is the bill? 200
 #What is the tip percentage? 15
 
