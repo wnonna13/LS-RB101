095de5a57a3e9e69fa7729165bdd308227fd620f
wnonna13 <wnonna@yahoo.com>
2023-05-08 05:15:34 -0400

    Add instructions

---
+++ small_problems/easy2/ex4.rb
@@ -0,0 +1,9 @@
+# Build a program that displays when the user will retire and how many years she has to work till retirement.
+
+#Example:
+
+#What is your age? 30
+#At what age would you like to retire? 70
+
+#It's 2016. You will retire in 2056.
+#You have only 40 years of work to go!
