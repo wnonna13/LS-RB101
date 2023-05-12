410239bec706f2c93b708e8400efdaa9c9db2a61
wnonna13 <wnonna@yahoo.com>
2023-05-12 14:14:04 -0400

    Add instructions

---
+++ small_problems/easy2/ex8.rb
@@ -0,0 +1,14 @@
+# Write a program that asks the user to enter an integer greater than 0, then asks if the user wants to determine the sum or product of all numbers between 1 and the entered integer.
+
+#>> Please enter an integer greater than 0:
+#5
+#>> Enter 's' to compute the sum, 'p' to compute the product.
+#s
+#The sum of the integers between 1 and 5 is 15.
+
+
+#>> Please enter an integer greater than 0:
+#6
+#>> Enter 's' to compute the sum, 'p' to compute the product.
+#p
+#The product of the integers between 1 and 6 is 720.
