d65b9edf458a1d2c6116c4834c2cac8e0a90daab
wnonna13 <wnonna@yahoo.com>
2023-05-12 14:09:44 -0400

    Add pseudocode

--- small_problems/easy2/ex7.rb
+++ small_problems/easy2/ex7.rb
@@ -1,1 +1,10 @@
 #Print all even numbers from 1 to 99, inclusive, to the console, with each number on a separate line.
+
+#PSEUDOCODE
+
+#input = none
+#output = even numbers on a separate line
+ 
+#Use a while loop (num < 99)
+    #If num %2 == 0 
+        # puts num
