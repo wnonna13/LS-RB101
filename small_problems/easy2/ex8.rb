33097105be3f3d2f4c14108c206ca3843ed854db
wnonna13 <wnonna@yahoo.com>
2023-05-13 04:51:01 -0400

    Add pseudocode method

--- small_problems/easy2/ex8.rb
+++ small_problems/easy2/ex8.rb
@@ -16,3 +16,17 @@
 #PSEUDOCODE
 #input =  integer greater than 0, s or p
 #output = sum or product
+
+# METHOD
+# Method 1: Ask user for input
+    # Validate it is a number
+        # ask if user is happy with the number
+            # If yes, proceed
+            # If no, ask for correct number
+
+# Print message explaining what "s" and "p" options are
+# Do Method 1 with s/p input 
+# Method 2: If s
+    # Add all the sum of integers from range (1..s)
+# Method 3: If p
+    # Add all the products of integers from range (1..p)
