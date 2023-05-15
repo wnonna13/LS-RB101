aa3197fcf20ff4cef3f02547c0a6265563ee0845
wnonna13 <wnonna@yahoo.com>
2023-05-15 07:53:58 -0400

    Add instructions

---
+++ small_problems/easy2/ex10.rb
@@ -0,0 +1,5 @@
+#Write a method that takes a number as an argument. If the argument is a positive number, return the negative of that number. If the number is 0 or negative, return the original number.
+
+negative(5) == -5
+negative(-3) == -3
+negative(0) == 0      # There's no such thing as -0 in ruby
