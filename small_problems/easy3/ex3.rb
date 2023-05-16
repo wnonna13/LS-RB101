3eebbd39ea2abae455620ae54bd8a578804de2b8
wnonna13 <wnonna@yahoo.com>
2023-05-16 07:17:58 -0400

    Add examples

--- small_problems/easy3/ex3.rb
+++ small_problems/easy3/ex3.rb
@@ -1,1 +1,7 @@
 #Write a program that will ask a user for an input of a word or multiple words and give back the number of characters. Spaces should not be counted as a character.
+
+#Please write word or multiple words: walk
+#There are 4 characters in "walk".
+
+#Please write word or multiple words: walk, don't run
+#There are 13 characters in "walk, don't run".
