af42064afe926316210fb19f6569504e5e5d494b
wnonna13 <wnonna@yahoo.com>
2023-05-16 07:15:29 -0400

    Add instructions

---
+++ small_problems/easy3/ex1.rb
@@ -0,0 +1,30 @@
+#Write a program that solicits 6 numbers from the user, then prints a message that describes whether or not the 6th number appears amongst the first 5 numbers.
+
+==> Enter the 1st number:
+25
+==> Enter the 2nd number:
+15
+==> Enter the 3rd number:
+20
+==> Enter the 4th number:
+17
+==> Enter the 5th number:
+23
+==> Enter the last number:
+17
+The number 17 appears in [25, 15, 20, 17, 23].
+
+
+==> Enter the 1st number:
+25
+==> Enter the 2nd number:
+15
+==> Enter the 3rd number:
+20
+==> Enter the 4th number:
+17
+==> Enter the 5th number:
+23
+==> Enter the last number:
+18
+The number 18 does not appear in [25, 15, 20, 17, 23].
