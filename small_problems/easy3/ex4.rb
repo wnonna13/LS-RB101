0af1eb534c4e8651bb9605f7b7e162a78cbb4776
wnonna13 <wnonna@yahoo.com>
2023-05-20 11:31:57 -0400

    Add pseudocode method

--- small_problems/easy3/ex4.rb
+++ small_problems/easy3/ex4.rb
@@ -1,8 +1,13 @@
-#Create a method that takes two arguments, multiplies them together, and returns the result.
+# Create a method that takes two arguments, multiplies them together, and returns the result.
 
 multiply(5, 3) == 15
 
-#PSEUDOCODE
+# PSEUDOCODE
 # INPUT = num1, num2
 # OUTPUT = result
 
+# METHOD
+# Get an integer from user
+# Get a second integer from user
+# Multiply int1 * int2 and save result as a new variable (as "result")
+# Return this new variable ("result")
