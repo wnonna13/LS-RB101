334d8b796d2e2cf11ac052d11be0d6a835b8361e
wnonna13 <wnonna@yahoo.com>
2023-05-21 06:50:44 -0400

    Add pseudocode input,output

--- small_problems/easy3/ex5.rb
+++ small_problems/easy3/ex5.rb
@@ -1,4 +1,9 @@
 #Using the multiply method from the "Multiplying Two Numbers" problem, write a method that computes the square of its argument (the square is the result of multiplying a number by itself).
 
+# PSEUDOCODE 
+# INPUT = input
+# OUTPUT =  product 
+
+
 square(5) == 25
 square(-8) == 64
