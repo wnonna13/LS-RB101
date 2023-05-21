5cef036096a172d67bdb7c2e661be4b483365195
wnonna13 <wnonna@yahoo.com>
2023-05-21 06:44:12 -0400

    Add instructions

---
+++ small_problems/easy3/ex5.rb
@@ -0,0 +1,4 @@
+#Using the multiply method from the "Multiplying Two Numbers" problem, write a method that computes the square of its argument (the square is the result of multiplying a number by itself).
+
+square(5) == 25
+square(-8) == 64
