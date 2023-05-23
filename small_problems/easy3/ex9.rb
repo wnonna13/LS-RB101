827ac5f855658f3b4a0cb530d33c26d197773461
wnonna13 <wnonna@yahoo.com>
2023-05-23 03:43:20 -0600

    Add instructions

---
+++ small_problems/easy3/ex9.rb
@@ -0,0 +1,6 @@
+#Write a method that returns true if its integer argument is palindromic, false otherwise. A palindromic number reads the same forwards and backwards.
+
+palindromic_number?(34543) == true
+palindromic_number?(123210) == false
+palindromic_number?(22) == true
+palindromic_number?(5) == true
