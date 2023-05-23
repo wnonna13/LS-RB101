da486fb4f432b8d6079354741a7240c7d0366d37
wnonna13 <wnonna@yahoo.com>
2023-05-23 03:30:08 -0600

    Add pseudocode input/output

--- small_problems/easy3/ex7.rb
+++ small_problems/easy3/ex7.rb
@@ -1,5 +1,10 @@
 #Write a method that returns true if the string passed as an argument is a palindrome, false otherwise. A palindrome reads the same forward and backward. For this exercise, case matters as does punctuation and spaces.
 
+#PSEUDOCODE
+
+# INPUT = string
+# OUTPUT = true OR false (depending on if it is a palindrome)
+
 palindrome?('madam') == true
 palindrome?('Madam') == false          # (case matters)
 palindrome?("madam i'm adam") == false # (all characters matter)
