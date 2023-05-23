df96a8d11ea28872f6f7391b2997de59858bc508
wnonna13 <wnonna@yahoo.com>
2023-05-23 03:43:45 -0600

    Add instructions

---
+++ small_problems/easy3/ex10.rb
@@ -0,0 +1,8 @@
+#Write a method that takes a string argument, and returns true if all of the alphabetic characters inside the string are uppercase, false otherwise. Characters that are not alphabetic should be ignored.
+
+uppercase?('t') == false
+uppercase?('T') == true
+uppercase?('Four Score') == false
+uppercase?('FOUR SCORE') == true
+uppercase?('4SCORE!') == true
+uppercase?('') == true
