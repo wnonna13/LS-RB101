c4826adb6efe4f3c840c4ce366e451fa4ee4870b
wnonna13 <wnonna@yahoo.com>
2023-05-25 05:49:58 -0400

    Create method

--- small_problems/easy3/ex10.rb
+++ small_problems/easy3/ex10.rb
@@ -1,5 +1,6 @@
 #Write a method that takes a string argument, and returns true if all of the alphabetic characters inside the string are uppercase, false otherwise. Characters that are not alphabetic should be ignored.
 
+
 uppercase?('t') == false
 uppercase?('T') == true
 uppercase?('Four Score') == false
--- small_problems/easy3/ex9.rb
+++ small_problems/easy3/ex9.rb
@@ -12,7 +12,14 @@
     # Return true
     # Else false
 
-palindromic_number?(34543) == true
-palindromic_number?(123210) == false
-palindromic_number?(22) == true
-palindromic_number?(5) == true
+def palindromic_number?(num)
+    num_s = num.to_s
+    new_s = num_s.reverse
+    (new_s == num_s)? true : false
+end
+
+palindromic_number?(34543) #== true
+palindromic_number?(123210) #== false
+palindromic_number?(22) #== true
+palindromic_number?(5) #== true
+
