57e924187057eac9f9e874cbeebfb7c00987107b
wnonna13 <wnonna@yahoo.com>
2023-05-26 08:01:23 -0400

    Change spacing

--- small_problems/easy3/ex8.rb
+++ small_problems/easy3/ex8.rb
@@ -15,22 +15,23 @@
 
 def real_palindrome?(str)
     new_str = " "
-    str.each_char { |c| 
-    if c.match(/\A[a-zA-Z0-9]*\z/).nil?
-        new_str += c
-    else
-        nil
+    str.each_char do |c| 
+        if c.match(/\A[a-zA-Z0-9]*\z/).nil?
+            new_str.push(c)
+        else
+            nil
+        end
     end
-    }
     (new_str == str)? true : false
+    puts new_str
 
 end
 
 
 
 real_palindrome?('madam') #== true
-real_palindrome?('Madam') #== true           # (case does not matter)
-real_palindrome?("Madam, I'm Adam") #== true # (only alphanumerics matter)
-real_palindrome?('356653') #== true
-real_palindrome?('356a653') #== true
-real_palindrome?('123ab321') #== false
+#puts real_palindrome?('Madam') #== true           # (case does not matter)
+#puts real_palindrome?("Madam, I'm Adam") #== true # (only alphanumerics matter)
+#puts real_palindrome?('356653') #== true
+#puts real_palindrome?('356a653') #== true
+#puts real_palindrome?('123ab321') #== false
