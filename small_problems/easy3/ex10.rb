7c991c31a2d14b8916e0cd37fcdf701911aa363e
wnonna13 <wnonna@yahoo.com>
2023-05-26 08:02:06 -0400

    Add pseudocode input, output

--- small_problems/easy3/ex10.rb
+++ small_problems/easy3/ex10.rb
@@ -1,5 +1,9 @@
 #Write a method that takes a string argument, and returns true if all of the alphabetic characters inside the string are uppercase, false otherwise. Characters that are not alphabetic should be ignored.
 
+# PSEUDOCODE
+# INPUT = string
+# OUTPUT = true or false
+
 
 uppercase?('t') == false
 uppercase?('T') == true
