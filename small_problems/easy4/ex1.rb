9243fd416646a39e0f8d66d7401175807dfc661e
wnonna13 <wnonna@yahoo.com>
2023-05-26 12:19:42 -0400

    Add input, output pseudocode

--- small_problems/easy4/ex1.rb
+++ small_problems/easy4/ex1.rb
@@ -1,5 +1,10 @@
 #Write a method that takes two strings as arguments, determines the longest of the two strings, and then returns the result of concatenating the shorter string, the longer string, and the shorter string once again. You may assume that the strings are of different lengths.
 
+# PSEUDOCODE 
+
+# Input = string1, string2
+# Output = string3
+
 short_long_short('abc', 'defgh') == "abcdefghabc"
 short_long_short('abcde', 'fgh') == "fghabcdefgh"
 short_long_short('', 'xyz') == "xyz"
