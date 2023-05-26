b21a1fc61e4a792dc0fbb86e36956f608c85f484
wnonna13 <wnonna@yahoo.com>
2023-05-26 12:18:30 -0400

    Add instructions

---
+++ small_problems/easy4/ex1.rb
@@ -0,0 +1,5 @@
+#Write a method that takes two strings as arguments, determines the longest of the two strings, and then returns the result of concatenating the shorter string, the longer string, and the shorter string once again. You may assume that the strings are of different lengths.
+
+short_long_short('abc', 'defgh') == "abcdefghabc"
+short_long_short('abcde', 'fgh') == "fghabcdefgh"
+short_long_short('', 'xyz') == "xyz"
