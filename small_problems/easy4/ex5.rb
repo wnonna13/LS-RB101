7819aa5a70122f94ccb3d7356d2f310a993ecb32
wnonna13 <wnonna@yahoo.com>
2023-05-29 06:44:04 -0400

    Add instructions

---
+++ small_problems/easy4/ex5.rb
@@ -0,0 +1,9 @@
+#Multiples of 3 and 5
+#Write a method that searches for all multiples of 3 or 5 that lie between 1 and some other number, and then computes the sum of those multiples. For instance, if the supplied number is 20, the result should be 98 (3 + 5 + 6 + 9 + 10 + 12 + 15 + 18 + 20).
+
+#You may assume that the number passed in is an integer greater than 1.
+
+multisum(3) == 3
+multisum(5) == 8
+multisum(10) == 33
+multisum(1000) == 234168
