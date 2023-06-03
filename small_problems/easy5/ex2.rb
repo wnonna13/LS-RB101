577e847735c2d06b690beab18d66a7c624d6ef1d
wnonna13 <wnonna@yahoo.com>
2023-06-03 08:26:56 -0400

    Add instructions

---
+++ small_problems/easy5/ex2.rb
@@ -0,0 +1,13 @@
+#The time of day can be represented as the number of minutes before or after midnight. If the number of minutes is positive, the time is after midnight. If the number of minutes is negative, the time is before midnight.
+
+#Write a method that takes a time using this minute-based format and returns the time of day in 24 hour format (hh:mm). Your method should work with any integer input.
+
+#You may not use ruby's Date and Time classes.
+
+time_of_day(0) == "00:00"
+time_of_day(-3) == "23:57"
+time_of_day(35) == "00:35"
+time_of_day(-1437) == "00:03"
+time_of_day(3000) == "02:00"
+time_of_day(800) == "13:20"
+time_of_day(-4231) == "01:29"
