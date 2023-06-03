b3fd7e09d22270bd0a4b9f5f2caba67ee188fe6c
wnonna13 <wnonna@yahoo.com>
2023-06-03 08:26:11 -0400

    Add instructions

---
+++ small_problems/easy5/ex1.rb
@@ -0,0 +1,6 @@
+#Write a method that determines and returns the ASCII string value of a string that is passed in as an argument. The ASCII string value is the sum of the ASCII values of every character in the string. (You may use String#ord to determine the ASCII value of a character.)
+
+ascii_value('Four score') == 984
+ascii_value('Launch School') == 1251
+ascii_value('a') == 97
+ascii_value('') == 0
