fe4e76a620d5b341d800cdadfc255f329043fb47
wnonna13 <wnonna@yahoo.com>
2023-07-24 12:38:14 +0700

    Add unshift method

--- small_problems/easy4/ex9.rb
+++ small_problems/easy4/ex9.rb
@@ -5,6 +5,33 @@
 #You may not use any of the standard conversion methods available in Ruby, such as Integer#to_s, String(), Kernel#format, etc. Your method should do this the old-fashioned way and construct the string by analyzing and manipulating the number.
 
 #Examples
-integer_to_string(4321) == '4321'
-integer_to_string(0) == '0'
-integer_to_string(5000) == '5000'
+
+DIGITS = {
+    0 => '0',
+    1 => '1',
+    2 => '2', 
+    3 => '3',
+    4 => '4',
+    5 => '5',
+    6 => '6',
+    7 => '7', 
+    8 => '8',
+    9 => '9'
+}
+
+def split(num)
+    result = []
+    while num > 0 
+        result.unshift(num % 10)
+        num /= 10
+    end
+    result
+end
+
+p split(4321)
+
+
+#integer_to_string(4321) == '4321'
+#integer_to_string(0) == '0'
+#integer_to_string(5000) == '5000'
+
