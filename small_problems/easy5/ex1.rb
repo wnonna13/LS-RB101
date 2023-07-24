7a7178fec521af8fb697c529b5d2af0c1d268c5d
wnonna13 <wnonna@yahoo.com>
2023-07-24 17:02:56 +0700

    Add pseudocode:

--- small_problems/easy5/ex1.rb
+++ small_problems/easy5/ex1.rb
@@ -1,5 +1,15 @@
 #Write a method that determines and returns the ASCII string value of a string that is passed in as an argument. The ASCII string value is the sum of the ASCII values of every character in the string. (You may use String#ord to determine the ASCII value of a character.)
 
+# input = string
+# output = number 
+# method 
+    # LOOP 
+    # for i in str
+        # total = 0
+        # total = get ascii code(i) + total
+        # return total
+
+
 ascii_value('Four score') == 984
 ascii_value('Launch School') == 1251
 ascii_value('a') == 97
