a53275405a791f10c8015984c880e094a59a8a43
wnonna13 <wnonna@yahoo.com>
2023-07-24 10:53:58 +0700

    Add compare_num()

--- small_problems/easy4/ex7.rb
+++ small_problems/easy4/ex7.rb
@@ -18,6 +18,21 @@ def split_string(str)
     str.split('')
 end       
 
+def compare_num(str)
+    case str
+    when "0" then 0
+    when "1" then 1 
+    when "2" then 2
+    when "3" then 3
+    when "4" then 4
+    when "5" then 5
+    when "6" then 6
+    when "7" then 7
+    when "8" then 8
+    when "9" then 9
+    end
+end
+
 puts split_string('4321')
 puts split_string('570')
 
