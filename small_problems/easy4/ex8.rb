1cd2e594714999fe4238fffb693ac75e75d6be95
wnonna13 <wnonna@yahoo.com>
2023-07-24 12:07:31 +0700

    Add first attempt

--- small_problems/easy4/ex8.rb
+++ small_problems/easy4/ex8.rb
@@ -7,6 +7,30 @@
 
 #You may not use any of the standard conversion methods available in Ruby, such as String#to_i, Integer(), etc. You may, however, use the string_to_integer method from the previous lesson.
 
+DIGITS = {
+    "0" => 0,
+    "1" => 1,
+    "2" => 2, 
+    "3" => 3,
+    "4" => 4,
+    "5" => 5,
+    "6" => 6,
+    "7" => 7,
+    "8" => 8,
+    "9" => 9,
+    "-" => 0,
+    "+" => 0
+}
+
+def string_to_signed_integer(str)
+    digits = str.chars.map { |i| DIGITS[i]}
+
+    value = 0
+    digits.each { |digit| value = 10 * value + digit}
+    value
+end
+
+
 string_to_signed_integer('4321') == 4321
 string_to_signed_integer('-570') == -570
 string_to_signed_integer('+100') == 100
