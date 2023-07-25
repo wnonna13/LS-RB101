47376261d70ff12f937d22c2f787438b8499559f
wnonna13 <wnonna@yahoo.com>
2023-07-25 14:24:23 +0700

    Add first gets

--- projects/calculator.rb
+++ projects/calculator.rb
@@ -3,5 +3,10 @@
 # perform the operation on the two numbers
 # output the result
 
-answer = Kernel.gets()
-Kernel.puts (answer)
+#answer = Kernel.gets()
+#Kernel.puts (answer)
+
+Kernel.puts("Welcome to Calculator!")
+
+Kernel.puts("What's the first number?")
+number1 = Kernel.gets()
