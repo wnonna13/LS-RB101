58a6bbd7afa14d04538cc096a51992375c0ee5d3
wnonna13 <wnonna@yahoo.com>
2023-07-26 20:15:03 +0700

    Add test casre

--- small_problems/easy5/ex5.rb
+++ small_problems/easy5/ex5.rb
@@ -1,1 +1,3 @@
 # Given a string that consists of some words (all lowercased) and an assortment of non-alphabetic characters, write a method that returns that string with all of the non-alphabetic characters replaced by spaces. If one or more non-alphabetic characters occur in a row, you should only have one space in the result (the result should never have consecutive spaces).
+
+cleanup("---what's my +*& line?") == ' what s my line '
