5a4042a9ccd3b1ccdb5c7aaa763fe3149f50c3e8
wnonna13 <wnonna@yahoo.com>
2023-07-26 19:47:43 +0700

    Add instructions

--- small_problems/easy5/ex4.rb
+++ small_problems/easy5/ex4.rb
@@ -1,3 +1,9 @@
+#Given a string of words separated by spaces, write a method that takes this string of words and returns a string in which the first and last letters of every word are swapped.
+
+#You may assume that every word contains at least one letter, and that the string will always contain at least one word. You may also assume that each string contains nothing but words and spaces.
+
+
+
 swap('Oh what a wonderful day it is') == 'hO thaw a londerfuw yad ti si'
 swap('Abcde') == 'ebcdA'
 swap('a') == 'a'
