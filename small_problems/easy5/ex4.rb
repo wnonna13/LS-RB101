b664263077616480281c9b52c407303c99f96f3a
wnonna13 <wnonna@yahoo.com>
2023-07-26 19:49:39 +0700

    Add pseudocode

--- small_problems/easy5/ex4.rb
+++ small_problems/easy5/ex4.rb
@@ -2,6 +2,16 @@
 
 #You may assume that every word contains at least one letter, and that the string will always contain at least one word. You may also assume that each string contains nothing but words and spaces.
 
+# input = string
+# output = string (but last and first word swapped)
+
+# method
+# split string into separate arrays
+# do for loop 
+    # first_letter = string[0]
+    # last_letter = string[-1]
+    # string[0] = last_letter
+    # string[-1] = first_letter
 
 
 swap('Oh what a wonderful day it is') == 'hO thaw a londerfuw yad ti si'
