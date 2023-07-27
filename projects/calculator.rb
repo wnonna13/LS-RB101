2317f7f0d5ba64889f288cafb6ec5299336b2d58
wnonna13 <wnonna@yahoo.com>
2023-07-27 10:15:49 +0700

    Delete if/elsif/else'

--- projects/calculator.rb
+++ projects/calculator.rb
@@ -11,3 +11,13 @@ Kernel.puts("Welcome to Calculator!")
 Kernel.puts("What's the first number?")
 number1 = Kernel.gets()
 Kernel.puts(number1)
+
+if operator == '1'
+    result = number1.to_i() + number2.to_i()
+elsif operator == '2'
+    result = number1.to_i() - number2.to_i()
+elsif operator == '3'
+    result = number1.to_i() * number2.to_i()
+else 
+    result = number1.to_f() / number2.to_f()
+end
--- projects/calculator_refactor.rb
+++ projects/calculator_refactor.rb
@@ -14,16 +14,6 @@ prompt("What operation would you like to perform? 1)add 2)subtract 3)multiply 4)
 
 operator = Kernel.gets().chomp()
 
-if operator == '1'
-    result = number1.to_i() + number2.to_i()
-elsif operator == '2'
-    result = number1.to_i() - number2.to_i()
-elsif operator == '3'
-    result = number1.to_i() * number2.to_i()
-else 
-    result = number1.to_f() / number2.to_f()
-end
-
 result = case operator
     when '1' then (number1.to_i() + number2.to_i())
     when '2' then (number1.to_i() - number2.to_i())
