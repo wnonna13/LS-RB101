5c619c560b561bdf17e505df1b6b6398d6740060
wnonna13 <wnonna@yahoo.com>
2023-07-27 10:38:09 +0700

    Fix validate()

--- projects/calculator_refactor.rb
+++ projects/calculator_refactor.rb
@@ -8,25 +8,29 @@ end
 
 
 def validate()
+    num = ''
     loop do 
         prompt("Please enter a number:")
         num = Kernel.gets().chomp()
 
-    if valid_number?(num)
-        break
-    else
-        prompt("Hmmm...that doesn't look like a valid number")
-    end
-    
+        if valid_number?(num)
+            break
+        else
+            prompt("Hmmm...that doesn't look like a valid number")
+        end
+    end   
+    num
 end
+
+number1 = ''
+number2 = ''
+
 
 number1 = validate()
 number2 = validate()
 
 prompt("Welcome to Calculator")
 
-number1 = ''
-number2 = ''
 
 #loop do 
 #    prompt("What's the first number?")
@@ -57,10 +61,13 @@ prompt("What operation would you like to perform? 1)add 2)subtract 3)multiply 4)
 
 operator = Kernel.gets().chomp()
 
+p number1
+p number2
+
 result = case operator
     when '1' then (number1.to_i() + number2.to_i())
     when '2' then (number1.to_i() - number2.to_i())
-    when '3' then (number1.to_i() * number2.to_i())
+    when '3' then (number1.to_f() * number2.to_f())
     when '4' then (number1.to_f() / number2.to_f())
 end
 
