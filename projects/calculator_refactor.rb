10c0a4a80ea349c3485d58bb0f313d723572155d
wnonna13 <wnonna@yahoo.com>
2023-07-27 10:10:57 +0700

    Update prompts

--- projects/calculator_refactor.rb
+++ projects/calculator_refactor.rb
@@ -10,7 +10,7 @@ number1 = Kernel.gets().chomp()
 prompt("What's the second number?")
 number2 = Kernel.gets().chomp()
 
-Kernel.puts("What operation would you like to perform? 1)add 2)subtract 3)multiply 4)divide")
+prompt("What operation would you like to perform? 1)add 2)subtract 3)multiply 4)divide")
 
 operator = Kernel.gets().chomp()
 
@@ -30,3 +30,5 @@ case operator
     when '3' then (number1.to_i() * number2.to_i())
     when '4' then (number1.to_f() / number2.to_f())
 end
+
+prompt("The result is #{result}")
